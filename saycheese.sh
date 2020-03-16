7129abe0cba006eb29c9d2c1fc6090257225970e
Technical Partha <62250916+parthade1234@users.noreply.github.com>
2020-03-16 20:37:36 +0530

    Update saycheese.sh

--- saycheese.sh
+++ saycheese.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # SayCheese v1.1
-# coded by: github.com/thelinuxchoice/saycheese
+# coded by: github.com/parthade1234/saycheese
 # If you use any part from this code, giving me the credits. Read the Lincense!
 
 trap 'printf "\n";stop' 2
